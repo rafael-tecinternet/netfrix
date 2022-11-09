c5573d38a577ec4d0d82e79f3f598088af13559c
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-11-09 11:23:26 -0300

    Atividade

--- src/screen/FormBusca.js
+++ src/screen/FormBusca.js
@@ -1,14 +1,58 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const FormBusca = () => {
+  const [texto, onChangeText] = React.useState();
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={estilos.container}>
+      <Text style={estilos.texto}>
+        Star Trek? O poderoso Chefão? A trilogia Senhor dos Anéis?
+      </Text>
+      <Text style={estilos.texto}>
+        Localize um filme que você viu ou gostaria de ver!
+      </Text>
+      <View style={estilos.viewProcurar}>
+        <Ionicons name="film" size={48} color="black" />
+        <TextInput
+          style={estilos.input}
+          onChangeText={onChangeText}
+          value={texto}
+          placeholder="Digite o filme..."
+        />
+      </View>
+      <Button title="PROCURAR" color="#5451a6" />
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  texto: {
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+  viewProcurar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 16,
+  },
+  input: {
+    borderWidth: 1,
+    padding: 8,
+    flex: 0.95,
+  },
+});
